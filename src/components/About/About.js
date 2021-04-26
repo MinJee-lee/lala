a0a96b0e52e19da61c2615cea59a6babe2cf99d1
black7amanda@gmail.com <black7amanda@gmail.com>
2021-04-26 15:35:19 +0900

    Dom error

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,17 +8,21 @@ class About extends Component {
         <div className="content">
           <div className="content--inner">
             
-            <h2>Project</h2>
+            <h2>Github</h2>
             <ul>
-              <li>About <a href="https://www.notion.so/About-8d5c7b76a2ba417d83e2b3f3b6df6452">About</a> </li>
-              <li>My <a href="https://github.com/MinJee-lee">github</a> </li>
-              <li>project <a href="https://www.notion.so/Spring-Security-89ea86cd73574443addf32c18e2de111">Spring Security</a> </li>
+              <li>Minjee's <a href="https://github.com/MinJee-lee">github</a> </li>
+              </ul>
+              <h2>Project</h2>
+            <ul>
+              <li>Spring-Security, Spring-Boot Project :  <a href="https://www.notion.so/Spring-Security-89ea86cd73574443addf32c18e2de111">Spring Security</a> </li>
+              <li>Spring-Boot Project :  <a href="https://github.com/MinJee-lee/ShoppingMall">Shopping Mall</a> </li>
             </ul>
-            <h2>Posts this project is mentioned</h2>
+            <h2> Minjee</h2>
             <ul>
+            <li>About <a href="https://www.notion.so/About-8d5c7b76a2ba417d83e2b3f3b6df6452">About</a> </li>
               <li><em>Coming soon....</em></li>
             </ul>
-          </div>
+          </div> 
         </div>
       </div>
     );
